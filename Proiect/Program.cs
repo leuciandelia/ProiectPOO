17f5411eaff96866b6d14c47ab7d01af07075e78
leuciandelia <dleucian12@gmail.com>
2025-01-16 00:42:56 +0200

    Am creat in programul principal un exemplu de utilizare a claselor dezvoltate in proiect

--- Proiect/Program.cs
+++ Proiect/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace Proiect;
+﻿using Proiect.Clase;
+
+namespace Proiect;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var student = new Student("Andrei Popa");
+
+        var disciplinaObligatorie = new DisciplinaObligatorie("Analiza Matematica", 1, 1);
+        disciplinaObligatorie.Note.Add(new NotaExamen(9));
+        disciplinaObligatorie.Note.Add(new NotaActivitate(6));
+        
+        var disciplinaOptionala=new DisciplinaObligatorie("TS2", 1, 1);
+        disciplinaOptionala.Note.Add(new NotaActivitate(7));
+        
+        student.InroleazaDisciplina(disciplinaObligatorie);
+        student.InroleazaDisciplina(disciplinaOptionala);
+        
+        student.VizualizareNote();
+        Console.WriteLine($"Media Anuala:{student.CalculeazaMediaAnuala(1)}");
+        
+        
+        
+        
     }
 }
