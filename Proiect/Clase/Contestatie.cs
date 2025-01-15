b3c718866ef81e8f99eb5aac9639528896f3dbd1
danajianu <danajianu4@gmail.com>
2025-01-16 00:29:36 +0200

    Am creat clasa Contestatie, in care am urmarit motivul si starea contestatiei si am facut o functie pentru a o rezolva.

---
+++ Proiect/Clase/Contestatie.cs
@@ -0,0 +1,39 @@
+﻿namespace Proiect.Clase;
+
+public class Contestatie
+{
+    public Nota NotaContestata { get; private set; }
+    public string Motiv{get; private set;}
+    public string Stare {get; private set;}
+
+    public Contestatie(Nota nota, string motiv)
+    {
+        NotaContestata = nota;
+        Motiv = motiv;
+        Stare = "in asteptare";
+    }
+
+    public void RezolvaContestaia(bool aprobat, double valoareNoua = 0)
+    {
+        if (aprobat && valoareNoua > 0)
+        {
+            NotaContestata.Valoare = valoareNoua;
+        }
+
+        if (aprobat)
+        {
+            Stare = "Aprobata";
+        }
+        else
+        {
+            {
+                Stare = "Respinsa";
+            }
+        }
+    }
+
+    public void AfiseazaStare()
+        {
+            Console.WriteLine($"Nota: {NotaContestata.Valoare},Stare: {Stare}, Motiv: {Motiv}");
+        }
+ }
